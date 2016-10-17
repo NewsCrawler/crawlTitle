7fc4bfe1ee2f1ba5f58ecbe85a6e3decca8c7605
Hyunjun529 <hyunjun529@gmail.com>
2016-10-18 02:18:53 +0900

    I've got you in my sight

--- newscrawler/src/main/java/net/p316/newscrawler/App.java
+++ newscrawler/src/main/java/net/p316/newscrawler/App.java
@@ -14,29 +14,11 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		URLGenerator URLgen = new URLGenerator();
         String url = URLgen.getTargetUrl(105);
-        
-        print("Fetching %s...", url);
 
         Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
-
-        print("\nMedia: (%d)", media.size());
-        for (Element src : media) {
-            if (src.tagName().equals("img"))
-                print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
-            else
-                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-        }
-
-        print("\nImports: (%d)", imports.size());
-        for (Element link : imports) {
-            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
-        }
-
+        
+        Elements links = doc.select("#main_content .type02 a[href]");
+        
         print("\nLinks: (%d)", links.size());
         for (Element link : links) {
             print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
